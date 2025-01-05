34828419331acf5c54c41bae1312b4f06e4f0994
Talha Dincer <talhadincer96@hotmail.com>
2025-01-05 19:26:56 +0300

    main class etc.

---
+++ src/main/java/com/talha/txnAppWithTestContainer/TxnAppWithTestContainerApplication.java
@@ -0,0 +1,13 @@
+package com.talha.txnAppWithTestContainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnAppWithTestContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxnAppWithTestContainerApplication.class, args);
+	}
+
+}
